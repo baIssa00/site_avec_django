739cef3013c8c96177086cd27d590eb6f0cc2599
issaba00 <issaba.00@gmail.com>
2022-05-31 10:14:40 +0000

    first commit

---
+++ contact/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+class Contact(models.Model):
+    prenom = models.CharField(max_length=150)
+    nom = models.CharField(max_length=50)
+    numero = models.CharField(max_length=100)
+    email = models.EmailField (max_length=200)
+    pub_date = models.DateTimeField('date published')
+    auteur = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self) -> str:
+        return f"{self.prenom} {self.nom}"
